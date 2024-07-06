61a820981831c9e9c979942e314cb0fd54982b31
DXLeGenD <arfathasif900@gmail.com>
2024-07-06 18:29:37 +0530

    Completed

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PIL import Image
+from PIL import Image, ImageTk
 import cv2
 import gridfs
 import face_recognition
@@ -100,7 +100,7 @@ remove_criminal_window_name=None
 
 
 #to upload details to mongoDB
-def store_data_to_mongodb(name, age, records, image_path):
+def store_data_to_mongodb(name, age, gender, dob, blood_group, father_name, mother_name, records, image_path):
     try:
         # Read the image file
         with open(image_path, 'rb') as f:
@@ -120,7 +120,7 @@ def store_data_to_mongodb(name, age, records, image_path):
             # Convert the encoding to a list for storage in MongoDB
             encoding_list = encoding.tolist()
             # Store the data along with the image encoding in the criminals collection
-            data = {'name': name, 'age': age,'records':records, 'image_id': file_id}
+            data = {'name': name, 'age': age, 'gender': gender, 'dob': dob, 'blood_group': blood_group, 'father_name': father_name, 'mother_name': mother_name, 'records': records, 'image_id': file_id, 'face_encoding': encoding_list}
             collection.insert_one(data)
             print("Data stored in MongoDB")
             return True
@@ -130,14 +130,21 @@ def store_data_to_mongodb(name, age, records, image_path):
     except Exception as e:
         print(f"Failed to store data: {e}")
         return False
+    
 
+#To get the details from each field of add_criminal_window
 def select_and_store_data():
     name = add_criminal_name.get()
     age = age_entry.get()
+    gender = gender_entry.get()
+    dob = dob_entry.get()
+    blood_group = blood_group_entry.get()
+    father_name = father_name_entry.get()
+    mother_name = mother_name_entry.get()
     records = records_text.get("1.0", tk.END).strip()
     image_path = filedialog.askopenfilename()
-    if name and age and image_path and records:
-        if store_data_to_mongodb(name, age,records, image_path):
+    if name and age and gender and dob and blood_group and father_name and mother_name and image_path and records:
+        if store_data_to_mongodb(name, age, gender, dob, blood_group, father_name, mother_name, records, image_path):
             messagebox.showinfo("Success", "Data stored successfully!")
         else:
             messagebox.showerror("Error", "Failed to store data.")
@@ -146,6 +153,8 @@ def select_and_store_data():
     add_criminal_window.destroy()
 
 
+
+#to delete a criminal using his name from the database
 def delete_image():
     name=remove_criminal_window_name.get()
     if not name:
@@ -170,14 +179,14 @@ def get_location():
         return None
 
 # Function to send SMS with criminal location and details
-def send_sms(location, details):
-    account_sid = 'ACe6f312ed161307877c3e6514d5bb3f55'
-    auth_token = '7c115cf3c483b3b8043987a1fd99a2e4'
+def send_sms(location, name):
+    account_sid = 'AC0fac06e439d75b2f588a2c8c4da3fba8'
+    auth_token = '0b4f16f5afe0ca78b4050f6fe34faac3'
     client = Client(account_sid, auth_token)
 
     lat, lon, city, country = location
     google_maps_link = f"https://www.google.com/maps?q={lat},{lon}"
-    message_body = f"Criminal Detected: {details}\nCurrent Location: {city}, {country} (Latitude: {lat}, Longitude: {lon}).\nGoogle Maps Link: {google_maps_link}"
+    message_body = f"Criminal Detected: {name}\nCurrent Location: {city}, {country} (Latitude: {lat}, Longitude: {lon}).\nGoogle Maps Link: {google_maps_link}"
 
     try:
         # Retrieve phone numbers from the database
@@ -198,21 +207,23 @@ def send_sms(location, details):
             # Send SMS to the nearest phone number
             message = client.messages.create(
                 body=message_body,
-                from_='+13148876858', 
-                to=nearest_phone_number
+                from_='+14158684503', 
+                to='+918088320670'
             )
-            print(f"Message sent to {nearest_phone_number}: {message.sid}")
+            print(f"Message sent to +918088320670: {message.sid}")
         else:
             print("No phone number found in the database.")
     except Exception as e:
         print(f"Error: {e}")
 
-# Function to detect criminal using camera
+#function to reset the detected criminals history
 def reset_detected_criminals():
     global detected_criminals
     while True:
         time.sleep(900)  # Sleep for 900 seconds (15 minutes)
         detected_criminals.clear()
+
+
 
 def detect_criminal():
     cap = cv2.VideoCapture(0)
@@ -236,17 +247,18 @@ def detect_criminal():
         face_locations, face_names = sfr.detect_known_faces(frame)
         for face_loc, name in zip(face_locations, face_names):
             y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-            # Writing Text above the frame
             cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-            # Drawing the frame
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-            
+
             if name not in detected_criminals:
                 detected_criminals.add(name)
+                # Display criminal details window immediately
                 criminal_details = f"Name: {name}"
                 location = get_location()
                 if location:
                     send_sms(location, criminal_details)
+                root.after(0, display_criminal_details_new_window, name)
+
 
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -255,13 +267,58 @@ def detect_criminal():
     cap.release()
     cv2.destroyAllWindows()
 
-import tkinter as tk
-from tkinter import messagebox
 
 
-# Function to open the add criminal window
+#to display the detected criminals Info for 15 seconds
+def display_criminal_details_new_window(name):
+    criminal = collection.find_one({'name': name})
+    if not criminal:
+        return
+
+    details_window = tk.Toplevel(root)
+    details_window.title("Criminal Details")
+    details_window.config(bg='#2c3e50')
+
+    details_frame = tk.Frame(details_window, bg='#34495e', padx=20, pady=20)
+    details_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+    title_label = tk.Label(details_frame, text=f"Details of {name}", font=("Helvetica", 20, "bold"), bg='#34495e', fg='white')
+    title_label.grid(row=0, column=0, columnspan=2, pady=10)
+
+    
+
+    labels = ["Name", "Age", "Gender", "DOB", "Blood Group", "Father's Name", "Mother's Name", "Records"]
+    values = [criminal.get('name', ''), criminal.get('age', ''), criminal.get('gender', ''), 
+              criminal.get('dob', ''), criminal.get('blood_group', ''), criminal.get('father_name', ''), 
+              criminal.get('mother_name', ''), criminal.get('records', '')]
+    
+    for i, (label, value) in enumerate(zip(labels, values)):
+        tk.Label(details_frame, text=f"{label}:", font=("Helvetica", 15), bg='#34495e', fg='white').grid(row=i+1, column=0, padx=10, pady=10, sticky="e")
+        tk.Label(details_frame, text=value, font=("Helvetica", 15), bg='#34495e', fg='white').grid(row=i+1, column=1, padx=10, pady=10, sticky="w")
+
+    # Load and display the image associated with the criminal
+    image_id = criminal.get('image_id', '')
+    if image_id:
+        try:
+            image_data = fs.get(image_id).read()
+            image = Image.open(io.BytesIO(image_data))
+            image = image.convert("RGB")
+            image = image.resize((200, 200), Image.ANTIALIAS)
+
+            img = ImageTk.PhotoImage(image)
+            img_label = tk.Label(details_frame, image=img)
+            img_label.image = img  
+            img_label.grid(row=len(labels)+1, column=0, columnspan=2, pady=10)
+        except Exception as e:
+            print(f"Error loading image for {name}: {e}")    
+
+    # Automatically close the details window after 15 seconds
+    details_window.after(15000, details_window.destroy)
+
+
+#GUI to add a criminal to the database
 def open_add_criminal_window():
-    global add_criminal_name, add_criminal_image_label, age_entry, records_text, add_criminal_window
+    global add_criminal_name, add_criminal_image_label, age_entry, records_text, gender_entry, dob_entry, blood_group_entry, father_name_entry, mother_name_entry, add_criminal_window
     add_criminal_window = tk.Toplevel(root)
     add_criminal_window.title("Add Criminal")
     add_criminal_window.config(bg='#2c3e50')
@@ -282,15 +339,42 @@ def open_add_criminal_window():
     age_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
     age_entry.grid(row=2, column=1, padx=10, pady=10)
 
+    gender_label = tk.Label(add_criminal_frame, text="Gender:", font=("Helvetica", 15), bg='#34495e', fg='white')
+    gender_label.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+    gender_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
+    gender_entry.grid(row=3, column=1, padx=10, pady=10)
+
+    dob_label = tk.Label(add_criminal_frame, text="DOB:", font=("Helvetica", 15), bg='#34495e', fg='white')
+    dob_label.grid(row=4, column=0, padx=10, pady=10, sticky="e")
+    dob_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
+    dob_entry.grid(row=4, column=1, padx=10, pady=10)
+
+    blood_group_label = tk.Label(add_criminal_frame, text="Blood Group:", font=("Helvetica", 15), bg='#34495e', fg='white')
+    blood_group_label.grid(row=5, column=0, padx=10, pady=10, sticky="e")
+    blood_group_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
+    blood_group_entry.grid(row=5, column=1, padx=10, pady=10)
+
+    father_name_label = tk.Label(add_criminal_frame, text="Father's Name:", font=("Helvetica", 15), bg='#34495e', fg='white')
+    father_name_label.grid(row=6, column=0, padx=10, pady=10, sticky="e")
+    father_name_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
+    father_name_entry.grid(row=6, column=1, padx=10, pady=10)
+
+    mother_name_label = tk.Label(add_criminal_frame, text="Mother's Name:", font=("Helvetica", 15), bg='#34495e', fg='white')
+    mother_name_label.grid(row=7, column=0, padx=10, pady=10, sticky="e")
+    mother_name_entry = tk.Entry(add_criminal_frame, font=("Helvetica", 15), width=30)
+    mother_name_entry.grid(row=7, column=1, padx=10, pady=10)
+
     records_label = tk.Label(add_criminal_frame, text="Records:", font=("Helvetica", 15), bg='#34495e', fg='white')
-    records_label.grid(row=3, column=0, padx=10, pady=10, sticky="ne")
+    records_label.grid(row=8, column=0, padx=10, pady=10, sticky="ne")
     records_text = tk.Text(add_criminal_frame, height=4, width=30, font=("Helvetica", 15))
-    records_text.grid(row=3, column=1, padx=10, pady=10)
+    records_text.grid(row=8, column=1, padx=10, pady=10)
 
-    save_btn = tk.Button(add_criminal_frame, text="Select Image And Upload Details", command=select_and_store_data, **btn_style)
-    save_btn.grid(row=5, column=0, columnspan=2, padx=10, pady=20, sticky="we")
+    save_btn = tk.Button(add_criminal_frame, text="Select Image And Upload Details", command=select_and_store_data, **btn_style1)
+    save_btn.grid(row=9, column=0, columnspan=2, padx=10, pady=20, sticky="we")
 
-    
+
+
+#GUI to delete a criminal from the database
 def open_remove_criminal_window():
     global open_remove_criminal, remove_criminal_window_name
     open_remove_criminal = tk.Toplevel(root)
@@ -313,55 +397,67 @@ def open_remove_criminal_window():
     age_entry1 = tk.Entry(remove_criminal_frame, font=("Helvetica", 15), width=30)
     age_entry1.grid(row=2, column=1, padx=10, pady=10)
 
-    remove_btn = tk.Button(remove_criminal_frame, text="Remove", command=delete_image, **btn_style)
+    remove_btn = tk.Button(remove_criminal_frame, text="Remove", command=delete_image, **btn_style1)
     remove_btn.grid(row=3, column=0, columnspan=2, padx=10, pady=20)
 
-# Create the main window
-root = tk.Tk()
-root.title("Criminal Identification")
-
-# Make the window fullscreen
-root.attributes('-fullscreen', True)
-
-# Set the background color
-root.config(bg='#2c3e50')
-
-# Create and configure the main frame
-frame = tk.Frame(root, bg='#34495e', padx=20, pady=20)
-frame.pack(expand=True, fill='both')
-
-# Create and place the title label
-title_label = tk.Label(frame, text="Criminal Identification", font=("Helvetica", 30, "bold"), bg='#34495e', fg='white')
-title_label.pack(pady=20)
-
-# Create and place the buttons
-btn_frame = tk.Frame(frame, bg='#34495e')
-btn_frame.pack(pady=20)
-
-btn_style = {
-    'font': ("Helvetica", 15),
-    'bg': '#2980b9',
-    'fg': 'white',
-    'activebackground': '#3498db',
+#button style for windows buttons
+btn_style1 = {
+    'font': ("Helvetica", 20),
+    'bg': '#F7E7DC',
+    'fg': 'black',
+    'activebackground': '#E6B9A6',
     'activeforeground': 'white',
     'bd': 0,
     'relief': 'flat',
     'width': 20,
     'height': 2
 }
+#The Main GUI of the project
 
-add_btn = tk.Button(btn_frame, text="Add Criminal", command=open_add_criminal_window, **btn_style)
-add_btn.grid(row=0, column=0, padx=20, pady=20)
+root = tk.Tk()
+root.title("Criminal Identification")
+root.geometry("1500x1000")
 
-delete_btn = tk.Button(btn_frame, text="Remove Criminal", command=open_remove_criminal_window, **btn_style)
-delete_btn.grid(row=1, column=0, padx=20, pady=20)
+# Load the background image
+bg_image = Image.open("background.jpg")  # Replace with your image path
+bg_image = bg_image.resize((1800, 1000), Image.ANTIALIAS)
+bg_photo = ImageTk.PhotoImage(bg_image)
 
-detect_btn = tk.Button(btn_frame, text="Detect Criminal", command=detect_criminal, **btn_style)
-detect_btn.grid(row=2, column=0, padx=20, pady=20)
+# Create a Canvas and add the background image
+canvas = tk.Canvas(root, width=1500, height=1000)
+canvas.pack(fill='both', expand=True)
+canvas.create_image(0, 0, image=bg_photo, anchor='nw')
+canvas.create_text(750, 100, text="Criminal Identification", font=("Helvetica", 80, "bold"), fill="#F7E7DC")# Create a title label directly on the canvas
+# title_label = tk.Label(root, text="Criminal Identification", font=("Helvetica", 40, "bold"), fg='white', bg='#2c3e50')
+# canvas.create_window(750, 100, window=title_label)  # Position title at the top center
 
-exit_btn = tk.Button(btn_frame, text="Exit", command=root.quit, **btn_style)
-exit_btn.grid(row=3, column=0, padx=20, pady=20)
+btn_style = {
+    'font': ("Helvetica", 20),
+    'bg': '#F7E7DC',
+    'fg': 'black',
+    'activebackground': '#E6B9A6',
+    'activeforeground': 'white',
+    'bd': 0,
+    'relief': 'flat',
+    'width': 20,
+    'height': 3
+}
 
-# Load encoding images
+# Create buttons
+add_btn = tk.Button(root, text="Add Criminal", command=open_add_criminal_window, **btn_style)
+remove_btn = tk.Button(root, text="Remove Criminal", command=open_remove_criminal_window, **btn_style)
+detect_btn = tk.Button(root, text="Detect Criminal", command=detect_criminal, **btn_style)
+exit_btn = tk.Button(root, text="Exit", command=root.quit, **btn_style)
+
+# Align buttons in a column
+buttons = [add_btn, remove_btn, detect_btn, exit_btn]
+start_y = 250
+spacing = 150  # Increase spacing to add bottom margin
+for i, btn in enumerate(buttons):
+    canvas.create_window(750, start_y + i * spacing, window=btn)
+
+
+
+
 sfr.load_encoding_images()
 root.mainloop()
